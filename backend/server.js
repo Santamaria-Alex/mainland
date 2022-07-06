5c893a4c6c83b04e94b73974442ea807d90bf52a
Alex Santamaria <santamariaalex146@gmail.com>
2022-07-05 19:51:04 -0500

    custom error handler

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 6000;
 
 const app = express();
 
+//middleware for
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/wSchRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
