1ff48cef0dfa6c69a9ec6ef9e677780aabb92a38
Alex Santamaria <santamariaalex146@gmail.com>
2022-07-05 22:04:05 -0500

    using express async handler

--- backend/Controllers/wSchController.js
+++ backend/Controllers/wSchController.js
@@ -1,35 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get wednesday schedule
 //@route GET /api/goals
 //@access Private
-const getSchedule = (req, res) => {
+const getSchedule = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get schedule" });
-};
+});
 
 //@desc     Set wednesday schedule
 //@route    POST /api/goals
 //@access   Private
-const setSchedule = (req, res) => {
+const setSchedule = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add text field.");
   }
 
   res.status(200).json({ message: "Set schedule" });
-};
+});
 
 //@desc     Update wednesday schedule
 //@route    PUT /api/goals/:id
 //@access   Private
-const updateSchedule = (req, res) => {
+const updateSchedule = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update schedule ${req.params.id}` });
-};
+});
 
 //@desc     Delete wednesday schedule
 //@route    DELETE /api/goals/:id
 //@access   Private
-const deleteSchedule = (req, res) => {
+const deleteSchedule = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete schedule ${req.params.id}` });
-};
+});
 
 module.exports = {
   getSchedule,
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,3 +1,4 @@
+//overwrite default express error handler
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode ? res.statusCode : 500;
 
